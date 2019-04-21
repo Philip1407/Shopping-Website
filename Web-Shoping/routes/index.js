e54a33bd53bc326c8c51bec99f7eb62df424216b
1612384 <1612384@student.hcmus.edu.vn>
2019-04-22 01:00:46 +0700

    update login page

--- Web-Shoping/routes/index.js
+++ Web-Shoping/routes/index.js
@@ -6,7 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('home', { title: 'Trang chủ' });
 });
 router.get('/myaccount', function(req, res, next) {
-  res.render('account', { title: 'Tài khoản' });
+  res.render('account', { title: 'Tài khoản' ,layout: 'account'});
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Đăng Nhập' ,layout: 'login'});
+});
+router.get('/forgotpassword', function(req, res, next) {
+  res.render('forgotpassword', { title: 'Quên mật khẩu' ,layout: 'forgotpassword'});
 });
 
 module.exports = router;
