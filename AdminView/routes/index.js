41e28b1c8e046897a3b6e20c5b678585e1d833ce
leuyennhi <43956696+leuyennhi@users.noreply.github.com>
2019-04-23 18:10:08 +0700

    Update layout statistics

--- AdminView/routes/index.js
+++ AdminView/routes/index.js
@@ -22,12 +22,31 @@ router.get('/orders', function(req, res, next) {
   res.render('orders', { title: 'Quản lý đơn hàng'});
 });
 
-router.get('/statistics', function(req, res, next) {
-  res.render('statistics', { title: 'Thống kê'});
+router.get('/statistics_day', function(req, res, next) {
+  res.render('statistics_day', { title: 'Thống kê'});
+});
+
+router.get('/statistics_week', function(req, res, next) {
+  res.render('statistics_week', { title: 'Thống kê'});
+});
+
+router.get('/statistics_month', function(req, res, next) {
+  res.render('statistics_month', { title: 'Thống kê'});
+});
+
+router.get('/statistics_quarter', function(req, res, next) {
+  res.render('statistics_quarter', { title: 'Thống kê'});
+});
+
+router.get('/statistics_year', function(req, res, next) {
+  res.render('statistics_year', { title: 'Thống kê'});
 });
 
 router.get('/bestseller', function(req, res, next) {
   res.render('bestseller', { title: 'Quản lý đơn hàng'});
 });
 
+router.get('/edit_info_admin', function(req,res,next) {
+  res.render('edit_info_admin', {title: 'Chỉnh sửa thông tin'});
+});
 module.exports = router;
