feb9337622b4ff48bcd583b6fac123bc228f1a63
leuyennhi <43956696+leuyennhi@users.noreply.github.com>
2019-05-20 18:29:34 +0700

    Create model order

---
+++ AdminView/models/orders.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var SchemaTypes = mongoose.Schema.Types;
+var Schema = mongoose.Schema;
+
+var OrderSchema = new Schema({
+    custom: { type: Schema.ObjectId, required: true },
+    day: {type: Date, required: true},
+    status: {type: String, required: true},
+    address:{type: String, required: true},
+    products:{type:Array, required: true},
+});
+
+// Virtual for this Order instance URL.
+OrderSchema
+.virtual('url')
+.get(function () {
+  return '/order/'+this._id;
+});
+
+// Export model.
+module.exports = mongoose.model('Order', OrderSchema);
