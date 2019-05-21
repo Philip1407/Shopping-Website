1a8157bd89dbcd02185b014c023909000aab2e8e
leuyennhi <43956696+leuyennhi@users.noreply.github.com>
2019-05-21 23:16:39 +0700

    update orders, orderdetail layout

--- AdminView/controllers/orders.js
+++ AdminView/controllers/orders.js
@@ -2,24 +2,37 @@ var Product = require('../models/products');
 var Order = require('../models/orders');
 var User = require('../models/users');
 
+function formatDate(date){
+  var dd = date.getDate();
+  var mm=date.getMonth()+1;
+  var yyyy=date.getFullYear();
+  if(dd < 10)
+    {
+	    dd = '0'+ dd;
+    }
+  if(mm < 10)
+  {
+	  mm = '0' + mm;
+  }
+  return dd+"/"+mm+"/"+yyyy;
+  
+}
 exports.orders_list = function(req, res){
   Order.find().exec(function(err, result) {
     
     setTimeout(function(){
-      console.log(result);
       res.render('orders/orders', { title: 'Quản lý đơn hàng', data:result}); 
     },10000);
 
     result.forEach((order)=>{
-      console.log(order.custom);
+      order['date'] = formatDate(order.day);
       User.findOne({_id: order.custom},{_id:0, username:1}).exec().then((name)=>{
-        order['custom']=name['username'];
-        console.log(name['name']);
+        order['customName']=name['username'];
       }).catch((err) => {
         console.log(err);
       });
-
-        order['total']=0;
+      
+      order['total']=0;
         order.products.forEach( element => {
           return Product.findOne({_id:element.product},{ _id:0, name:1, price:1, }).exec().then((result) => {
             order['total']+= element.amount*result['price'];
@@ -47,19 +60,18 @@ exports.orders_delete_post = function(req, res) {
 exports.orders_getdetail = function(req,res, next) {
   total=0;
   Order.findOne({_id:req.params.id}, function(err, order) {
+    if(err){ return next(err); }
+
     setTimeout(function(){
-      console.log(order);
-      console.log(total);
       res.render('orders/orders_detail', { title: 'Chi tiết đơn hàng', data:order, total:total}); 
     },10000);
+    order['date'] = formatDate(order.day);
     User.findOne({_id: order.custom},{_id:0, username:1}).exec().then((name)=>{
-      order['custom']=name['username'];
-      console.log(name['username']);
+      order['customName']=name['username'];
     }).catch((err) => {
       console.log(err);
     });
 
-    if(err){ return next(err); }
     order.products.forEach( element => {
       return Product.findOne({_id:element.product},{ _id:0, name:1, price:1, }).exec().then((result) => {
         
