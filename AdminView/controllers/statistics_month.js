4fced5d7d6f83437f3ff0f06551a1d0950f0b13a
leuyennhi <43956696+leuyennhi@users.noreply.github.com>
2019-05-21 13:25:40 +0700

    update statistics_month controller, hbs

--- AdminView/controllers/statistics_month.js
+++ AdminView/controllers/statistics_month.js
@@ -1,7 +1,105 @@
-//var statistics_month = require('..\views\statistics_month');
+function getMonth(data) {
+  console.log(data);
+  var months = [];
+  data.forEach((item, index) => {
+      months[index]=item._id;
+      switch (months[index]) {
+        case 1:
+            months[index]="Tháng 1";
+            break; 
+        case 2:
+            months[index]="Tháng 2";
+            break;            
+        case 3:
+            months[index]="Tháng 3";
+            break;
+        case 4:
+          months[index]="Tháng 4";
+            break; 
+        case 5:
+            months[index]="Tháng 5";
+            break;            
+        case 6:
+            months[index]="Tháng 6";
+            break;
+        case 7:
+            months[index]="Tháng 7";
+            break; 
+        case 8:
+            months[index]="Tháng 8";
+            break;            
+        case 9:
+            months[index]="Tháng 9";
+            break;
+        case 10:
+            mnth="Tháng 10";
+            break; 
+        case 11:
+            months[index]="Tháng 11";
+            break;            
+        case 12:
+            months[index]="Tháng 12";
+            break;
+    }
+  });
+  return months;
+}
+
+function getValue(data) {
+  var value = [];
+  data.forEach((item, index) => {
+      value[index]=item.total;
+  });
+  
+  return value;
+}
+
+var Product = require('../models/products');
+var Order = require('../models/orders');
 
 exports.statistics_month_list = function(req, res){
-  res.render('statistics_month/statistics_month', { title: 'Thống kê'});
+  var current = new Date();
+  var year = parseInt(current.getFullYear());
+  var temp = null;
+  Order.aggregate(
+    [
+      {
+        $match: {
+        day: {$gte: new Date(year+'-01-01')}
+        }
+      },
+      {
+        $group: {
+          _id: {$month: '$day'},
+          product_list: { $push: "$products.product" },
+          amount_list: {$push: "$products.amount"}
+        }
+      }
+    ]
+ ).exec().then((list) => {
+  temp = list;
+  setTimeout(function(){
+    var result = [];
+    result[0] = getMonth(temp);
+    result[1] = getValue(temp);
+    console.log(result[0]);
+    res.render('statistics_month/statistics_month', { title: 'Thống kê', data: result});
+  },10000);
+    temp.forEach( async element => {
+      await element.product_list.forEach(async (item, index1) => {
+        element['total'] = 0;
+        await item.forEach((pro, index2) => {
+          return Product.findOne({_id:pro},{ _id:0, price:1}).exec().then((result) => {
+            //console.log(item);
+            element['total']+= element.amount_list[index1][index2]*result['price'];
+           // console.log(element['total']);
+          }).catch((err) => {
+            console.log(err);
+            });
+        });
+      });
+    });
+  });
 };
 
 // Display statistics_month update form on GET.
--- AdminView/controllers/statistics_year.js
+++ AdminView/controllers/statistics_year.js
@@ -1,11 +1,42 @@
 //var statistics_year = require('..\views\statistics_year');
+var Product = require('../models/products');
+var Order = require('../models/orders');
 
 exports.statistics_year_list = function(req, res){
-  res.render('statistics_year/statistics_year', { title: 'Thống kê'});
+  var current = new Date();
+  var year = parseInt(current.getFullYear());
+  
+  Order.find(
+    {day:{$gt: new Date(year+'-01-01')}}, 
+    {_id:0, day: 1, products:1}
+  ).exec().then((list) => {
+    console.log(list);
+    console.log(list[0].product[0]);
+    res.render('statistics_year/statistics_year', { title: 'Thống kê'});
+  });
+/*
+    temp = list_products;
+    temp.forEach( element => {
+      return Product.findOne({_id:element._id},{ _id:0, name: 1, catergory:1}).exec().then((result) => {
+        element['name'] = result['name'];
+        element['category']=result['catergory'];
+        Category.findOne({_id:element.category},{ _id:0, name: 1}).exec().then((result) => {
+          element['categoryName']=result['name'];
+        //console.log("123456");
+      }).catch((err) => {
+        console.log(err);
+      });
+    });
+  });
+ 
+});
+
+  res.render('statistics_year/statistics_year', { title: 'Thống kê'}); */
 };
 
 // Display statistics_year update form on GET.
 exports.statistics_year_update_get = function(req, res) {
+
 res.send('NOT IMPLEMENTED: statistics_year update GET');
 };
 
