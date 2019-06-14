8c773acf9712c44221cd00c611e849a5d37e8c16
sherlock4593 <1612384@student.hcmus.edu.vn>
2019-06-14 14:58:43 +0700

    update large image upload

--- Web-Shoping/app.js
+++ Web-Shoping/app.js
@@ -27,7 +27,7 @@ const cloudinary = require("cloudinary");
 const cloudinaryStorage = require("multer-storage-cloudinary");
 cloudinary.config({cloud_name: 'dpsdkyleb',api_key:'649252332669658',api_secret: '8ZfEWNU8eJGybHofn3lUya2qdVk'});
 
-const storage = cloudinaryStorage({cloudinary: cloudinary,folder: "demo",allowedFormats: ["jpg", "png"],transformation: [{ width: 500, height: 500, crop: "limit" }]});
+const storage = cloudinaryStorage({cloudinary: cloudinary,folder: "demo",allowedFormats: ["jpg", "png"],transformation: [{ width: 5000, height: 5000, crop: "limit" }]});
 
 const parser = multer({ storage: storage });
 
